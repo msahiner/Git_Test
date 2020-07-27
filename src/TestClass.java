2bb9223dd56b1edc90037c89458418fba82fa01a
msahiner <68484262+msahiner@users.noreply.github.com>
2020-07-27 03:20:19 -0400

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,5 +3,6 @@ import javax.tools.StandardLocation;
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Hello from Git");
+        System.out.println("Hello from GitHub");
     }
 }
